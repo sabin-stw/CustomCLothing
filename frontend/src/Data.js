48e6e0511cd7e244f7b513d1c7d9ad38b94e36b9
Sabin Simkhada <sbnsim81@gmail.com>
2021-05-26 14:27:47 +0545

    Second Commit

---
+++ frontend/src/Data.js
@@ -0,0 +1,78 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nepali Botique Kurthi',
+            category:'Kurthi',
+            image:'/images/p1.jpg',
+            price: 130,
+            brand:'Nepali',
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Kurthi',
+
+        },
+        {
+            _id:'2',
+            name:'Nepali Clothing',
+            category:'Kurthi',
+            image:'/images/p7.jpg',
+            price: 130,
+            brand:'Nepali',
+            rating:4.5,
+            numReviews:5,
+            description:'High Quality Kurthi',
+
+        },
+        {
+            _id:'3',
+            name:'Nepali Custom Kurthi',
+            category:'Kurthi',
+            image:'/images/p3.jpg',
+            price: 120,
+            brand:'Nepali',
+            rating:4.5,
+            numReviews:20,
+            description:'High Quality Kurthi',
+
+        },
+        {
+            _id:'4',
+            name:'Custom Botique Kurthi',
+            category:'Kurthi',
+            image:'/images/p4.jpg',
+            price: 145,
+            brand:'Nepali',
+            rating:4.5,
+            numReviews:11,
+            description:'High Quality Kurthi',
+
+        },
+        {
+            _id:'5',
+            name:'Botique Kurthi',
+            category:'Kurthi',
+            image:'/images/p5.jpg',
+            price: 135,
+            brand:'Nepali',
+            rating:4.5,
+            numReviews:15,
+            description:'High Quality Kurthi',
+
+        },
+        {
+            _id:'6',
+            name:'Nepali Kurthi',
+            category:'Kurthi',
+            image:'/images/p1.jpg',
+            price: 130,
+            brand:'Nepali',
+            rating:4.5,
+            numReviews:15,
+            description:'High Quality Kurthi',
+
+        },
+        
+    ],
+};
+export default data;
